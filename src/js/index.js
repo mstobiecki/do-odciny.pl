d1f9df06d82595fa4bf23284491e1d8504b5c3ac
Mateusz Stobiecki <m100biecki@gmail.com>
2023-05-12 19:16:38 +0200

    added JS in acordion section and finished section on mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,34 @@
 const navigation = document.querySelector('.navigation');
 const hamburgerNavigation = document.querySelector('.btn-mobile-nav');
+const accordion = document.querySelector('.accordion');
+const allElementsAccordion = document.querySelectorAll(
+	'.accordion__description'
+);
 
 const navigationMenu = function () {
 	navigation.classList.toggle('navigation__open--js');
 };
 
+const accordionMenu = function (e) {
+	const clicked = e.target.closest('.accordion__item');
+	const accordionDescription = clicked?.querySelector(
+		'.accordion__description'
+	);
+
+	if (!clicked) return;
+
+	document
+		.querySelector(
+			`.accordion__description--${accordionDescription.dataset.tab}`
+		)
+		.classList.toggle('accordion__open--js');
+
+	clicked
+		?.querySelector('.accordion__icon')
+		.firstChild.classList.toggle('accordion__rotate--js');
+
+	clicked.classList.toggle('accordion__border--js');
+};
+
 hamburgerNavigation.addEventListener('click', navigationMenu);
+accordion.addEventListener('click', accordionMenu);
